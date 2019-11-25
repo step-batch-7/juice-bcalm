f8291f46d97bea5b52d4261dcd2ff473cf0c22eb
Vikram <58028680+bcalm@users.noreply.github.com>
2019-11-25 14:52:05 +0530

    some name refactoring
    remove generateBeverageInfo fn

--- src/generateMessage.js
+++ src/generateMessage.js
@@ -5,17 +5,16 @@ const generateSaveMessage = function(empId, beverageName, qty, date) {
   return message;
 };
 
-const generateBeverageInfo = function(empId, transaction) {
-  return "\n" + empId + "," + transaction + "\n";
-};
-
 const generateQueryMessage = function(empId, queryDetails, count) {
   let message = "Employee ID, Beverage, Quantity, Date";
-  message += queryDetails.reduce(generateBeverageInfo, empId);
-  message += "Total:" + count + " juices";
+  message += queryDetails
+    .map(function(transaction) {
+      return "\n" + empId + "," + transaction;
+    })
+    .join(" ");
+  message += "\n" + "Total:" + count + " juices";
   return message;
 };
 
-exports.generateBeverageInfo = generateBeverageInfo;
 exports.generateQueryMessage = generateQueryMessage;
 exports.generateSaveMessage = generateSaveMessage;
